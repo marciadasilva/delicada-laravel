9723014e1cd197f82bd428a5ac8fc55c3e50d1d7
marcia <marcia.dasilva1410@gmail.com>
2017-11-17 17:28:04 -0200

    efeitos.js modificado

--- public/js/efeitos.js
+++ public/js/efeitos.js
@@ -1,9 +1,19 @@
-var $w = $(window);
+jQuery(document).ready(function($) {
+    var url_atual = window.location.href;
 
-$w.on("scroll", function(){
-    if( $w.scrollTop() < 50 ) {
-        $("nav").removeClass("cor-menu");
-    } else if( $w.scrollTop() > 50 ) {
+    // Código para que apenas a página inicial fique sem a cor de fundo da barra de menu
+    if(!url_atual.endsWith("/")){
         $("nav").addClass("cor-menu");
     }
+
+    var $w = $(window);
+
+    $w.on("scroll", function () {
+        if (($w.scrollTop() < 50 ) && (url_atual.endsWith("/"))) {
+            $("nav").removeClass("cor-menu");
+        } else if ($w.scrollTop() > 50) {
+            $("nav").addClass("cor-menu");
+        }
+    });
+
 });
