9069b02cb9ad0cec1936c520fb6a72b097090d2f
marcia <marcia.dasilva1410@gmail.com>
2017-11-17 22:36:43 -0200

    card products model pronto

--- resources/views/site/produtos.blade.php
+++ resources/views/site/produtos.blade.php
@@ -13,15 +13,96 @@
 </nav>
 
 <div class="container">
-    <div class="row" style="width: 50rem;">
-      <div class="card cards">
-        <img class="card-img-top" src="/img/golden.jpeg" alt="Card image cap">
-        <div class="card-body">
-          <h4 class="card-title">Card title</h4>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="row">
+      <div class="col-md-3 col-sm-6">
+        <div class="card">
+
+          <div class="size-card">
+            <img class="size-img" src="/img/summer.jpeg" alt="Card image cap">
+
+          </div>
+
+          <h2><a href="#">Nome do Produto</a></h2>
+
+          <div class="price-card">
+            <ins>R$ 3535</ins>
+          </div>
+
+          <div class="shop-card">
+            <a class="" data-quantity="1" data-product_sku=""
+               data-product_id="70" rel="nofollow" href="#">Comprar</a>
+          </div>
+
         </div>
+
       </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="card">
+
+          <div class="size-card">
+            <img class="size-img" src="/img/meme.png" alt="Card image cap">
+          </div>
+
+          <h2><a href="#">Nome do Produto</a></h2>
+
+          <div class="price-card">
+            <ins>R$ 3535</ins>
+          </div>
+
+          <div class="shop-card">
+            <a class="" data-quantity="1" data-product_sku=""
+               data-product_id="70" rel="nofollow" href="#">Comprar</a>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="card">
+
+          <div class="size-card">
+            <img class="size-img" src="/img/summer.jpeg" alt="Card image cap">
+          </div>
+
+          <h2><a href="#">Nome do Produto</a></h2>
+
+          <div class="price-card">
+            <ins>R$ 3535</ins>
+          </div>
+
+          <div class="shop-card">
+            <a class="" data-quantity="1" data-product_sku=""
+               data-product_id="70" rel="nofollow" href="#">Comprar</a>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="card">
+
+          <div class="size-card">
+            <img class="size-img" src="/img/meme.png" alt="Card image cap">
+          </div>
+
+          <h2><a href="#">Nome do Produto</a></h2>
+
+          <div class="price-card">
+            <ins>R$ 3535</ins>
+          </div>
+
+          <div class="shop-card">
+            <a class="" data-quantity="1" data-product_sku=""
+               data-product_id="70" rel="nofollow" href="#">Comprar</a>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
 </div>
 
