d8d2cb2e319f86bc060939ba78d11c36de4b9fb6
marcia <marcia.dasilva1410@gmail.com>
2017-12-01 18:28:35 -0200

    protecao menu

--- resources/views/layouts/side-menu.blade.php
+++ resources/views/layouts/side-menu.blade.php
@@ -1,5 +1,13 @@
 <ul class="collection">
-    <li class="collection-item"> <a href="/products">  Products  </a></li>
+    @if(isset($categories))
+        @if(count($categories))
+            <li class="collection-item"> <a href="/products">  Products  </a></li>
+        @else
+            <li class="collection-item"> <a style="color: #ccc">  Products  </a></li>
+        @endif
+    @else
+        <li class="collection-item"> <a href="/products">  Products  </a></li>
+    @endif
     <li class="collection-item"> <a href="/categories">Categories</a></li>
     <li class="collection-item"> <a href="/users">     Users     </a></li>
     <li class="collection-item"> <a href="/logout">    Logout    </a></li>
--- routes/web.php
+++ routes/web.php
@@ -3,52 +3,50 @@
 
 Route::get('/', function () { return view('site.index'); })->name('home');
 
-Route::namespace('Admin')->group(function () {
 // Register Routes
-    Route::get('/register', 'RegistrationController@create');
+Route::get('/register', 'RegistrationController@create');
 
-    Route::post('/register', 'RegistrationController@store');
+Route::post('/register', 'RegistrationController@store');
 
 // Login Routes
-    Route::get('/login', 'SessionsController@create')->name('login');
+Route::get('/login', 'SessionsController@create')->name('login');
 
-    Route::post('/login', 'SessionsController@store');
+Route::post('/login', 'SessionsController@store');
 
-    Route::get('/logout', 'SessionsController@destroy'); //testar com POST
+Route::get('/logout', 'SessionsController@destroy'); //testar com POST
 
 // Category Routes
 
-    Route::get('/category/create', 'CategoriesController@create');
+Route::get('/category/create', 'CategoriesController@create');
 
-    Route::post('/categories', 'CategoriesController@store');
+Route::post('/categories', 'CategoriesController@store');
 
-    Route::get('/categories', 'CategoriesController@index')->name('categories');
+Route::get('/categories', 'CategoriesController@index')->name('categories');
 
-    Route::get('/categories/{category}', 'CategoriesController@show');
+Route::get('/categories/{category}', 'CategoriesController@show');
 
-    Route::post('/categories/{category}', 'CategoriesController@update');
+Route::post('/categories/{category}', 'CategoriesController@update');
 
-    Route::get('/categories/delete/{category}', 'CategoriesController@destroy');
+Route::get('/categories/delete/{category}', 'CategoriesController@destroy');
 
 // Product Routes
 
-    Route::get('/product/create', 'ProductsController@create');
+Route::get('/product/create', 'ProductsController@create');
 
-    Route::get('/products', 'ProductsController@index')->name('products');
+Route::get('/products', 'ProductsController@index')->name('products');
 
-    Route::post('/products', 'ProductsController@store');
+Route::post('/products', 'ProductsController@store');
 
-    Route::get('/products/{product}', 'ProductsController@show');
+Route::get('/products/{product}', 'ProductsController@show');
 
-    Route::get('/products/{product}', 'ProductsController@edit');
+Route::get('/products/{product}', 'ProductsController@edit');
 
-    Route::post('/products/{product}', 'ProductsController@update');
+Route::post('/products/{product}', 'ProductsController@update');
 
-    Route::get('/products/delete/{product}', 'ProductsController@destroy');
+Route::get('/products/delete/{product}', 'ProductsController@destroy');
 
 // User Routes
 
-    Route::get('/users', 'UserController@index')->name('users');
+Route::get('/users', 'UserController@index')->name('users');
 
-    Route::get('/users/{user}', 'UserController@show');
-});
+Route::get('/users/{user}', 'UserController@show');
